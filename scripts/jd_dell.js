5babd26d102875f1e1c0df99aeaa83d72ce91604
star <869728223@qq.com>
2021-10-13 17:06:23 +0800

    去除内置助力码，增加活动京豆统计，实物推送

--- scripts/jd_dell.js
+++ scripts/jd_dell.js
@@ -9,6 +9,7 @@
 * */
 const $ = new Env('戴尔');
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
+const notify = $.isNode() ? require('./sendNotify') : '';
 let cookiesArr = [];
 if ($.isNode()) {
     Object.keys(jdCookieNode).forEach((item) => {
